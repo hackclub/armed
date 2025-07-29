6c1ab188cb1a853c0e50804aa6cc626feaf8cbca
vandorena <alexvd@hackclub.com>
2025-07-28 21:39:39 -0400

    added epilepsy safe mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,33 @@
 	let { children } = $props();
 
 	let epilepsyWarning = $state(true);
+	let epilepsySafeMode = $state(false);
 
 	function epilepsyWarningDismiss() {
 		epilepsyWarning = false;
+	}
+
+	function EpilepsySafeMode() {
+		epilepsyWarning = false;
+		epilepsySafeMode = true;
 	}
 
 </script>
 
 {#if epilepsyWarning}
 	<div class="epilepsy-warning">
-		<p>This site contains flashing images that may trigger seizures for people with photosensitive epilepsy. Proceed with caution.</p>
-		<button onclick={epilepsyWarningDismiss}>I understand, proceed</button>
+		<p><b>Warning:</b> <u>This site contains flashing images that may trigger seizures for people with photosensitive epilepsy. Proceed with caution.</u></p>
+		<button onclick={epilepsyWarningDismiss} class="buttonProceed">I understand, proceed</button>
+		<button onclick={EpilepsySafeMode} class="buttonSafe">Turn on Epilepsy Safe Mode</button>
 	</div>
 {:else}
-	<div class="overlay" style="z-index: 0"></div>
-	<div class="scanline" style="z-index: 0"></div>
+	{#if epilepsySafeMode}
+		<div class="overlayES" style="z-index: 0"></div>
+		<div class="scanline" style="z-index: 0"></div>
+	{:else}
+		<div class="overlay" style="z-index: 0"></div>
+		<div class="scanline" style="z-index: 0"></div>
+	{/if}
 	<div class="wrapper">
 		{@render children()}
 	</div>
