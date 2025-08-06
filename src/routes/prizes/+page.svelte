a9b18e64d0f8ee2b6b66970fb611c6248e57da78
alexvd@hackclub.com <dinobox2@review-laptop-macs-MacBook-Air.local>
2025-08-06 13:01:02 -0400

    prizes update

--- src/routes/prizes/+page.svelte
+++ src/routes/prizes/+page.svelte
@@ -11,11 +11,11 @@
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⠿⠿⠿⠿⠿⠿⠟⠉⠀⠀<br>
 ⠀⠀⠀⠀⠀⠀⣀⣀⢀⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡀⣀⣀<br>
 ⠀⠀⠀⠀⢀⣼⣿⡿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢿⣿⣧⡀<br>
-⠀⠀⠀⢀⣾⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢸⣿⣿⣷.⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀- Minifigures are awarded at a rate of 1 Minifigure per hour<br>
-⠀⠀⠀⣼⣿⣿⣿⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ of development.<br>
-⠀⠀⣸⣿⣿⣿⡏⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢹⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀- Once you submit your project, you will receive your Minifigures<br>
-⠀⠀⣿⣿⣿⣿⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀based on the median hours logged by all participants.<br>
-⠀⠀⣿⣿⣿⡿⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⢿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀- Minifigures may have spare arms included.<br>
+⠀⠀⠀⢀⣾⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢸⣿⣿⣷.⠀⠀⠀- Minifigures are awarded at a rate of 1 Minifigure per hour<br>
+⠀⠀⠀⣼⣿⣿⣿⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⣿⣿⣿⣧⠀⠀⠀⠀⠀of development.<br>
+⠀⠀⣸⣿⣿⣿⡏⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢹⣿⣿⣿⣇⠀⠀- Once you submit your project, you will receive your Minifigures<br>
+⠀⠀⣿⣿⣿⣿⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⠀⠀⠀⠀based on the median hours logged by all participants.<br>
+⠀⠀⣿⣿⣿⡿⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⢿⣿⣿⣿⠀⠀- Minifigures may have spare arms included.<br>
 ⠀⣠⣿⣿⣿⡇⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⣿⣿⣿⣄<br>
 ⣾⡿⠋⠉⢻⣷⡀⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⢀⣾⡟⠉⠙⢿⣷<br>
 ⢿⡇⠀⠀⢸⣿⠁⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠈⣿⡇⠀⠀⢸⡿<br>
