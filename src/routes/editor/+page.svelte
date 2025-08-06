13d7fbccae1620c2c7e3c62a04788ef8ba666495
alexvd@hackclub.com <dinobox2@review-laptop-macs-MacBook-Air.local>
2025-08-06 14:16:57 -0400

    udpdaed editor

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+    let easyMode = $state(false);
+
+    function toggleEasyMode() {
+        easyMode = !easyMode;
+    }
+</script>
 <div class="min-h-screen flex justify-center items-center p-8 relative z-10">
     <a href="/">
         <button class="absolute top-5 right-10 hover:underline">
@@ -9,12 +16,26 @@
             [ Submit ]
         </button>
     </a>
+    <button class="absolute top-5 right-80 hover:underline" on:click={toggleEasyMode}>
+        {easyMode ? '[ Hard Mode ]' : '[ Easy Mode ]'}
+    </button>
     <a href="https://peterhigginson.co.uk/ARMlite/Programming%20reference%20manual_v1_3.pdf">
-        <button class="absolute top-5 left-80 hover:underline">
+        <button class="absolute top-5 left-80 hover:underline" target="_blank">
             [ Instructionset Guide ]
         </button>
     </a>
-    <div class="w-full max-w-8xl h-[1000px] relative z-20">
+        {#if easyMode}
+        <div class="w-full max-w-4xl h-[900px] relative z-20">
+         <iframe 
+            src="https://www.peterhigginson.co.uk/AQA/?F5=06-Aug-25_17:58:56" 
+            class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
+            title="ArmLite"
+            allow="fullscreen"
+            loading="lazy"
+        ></iframe>
+         </div>
+        {:else}
+        <div class="w-full max-w-8xl h-[1000px] relative z-20">
         <iframe 
             src="https://peterhigginson.co.uk/ARMlite/" 
             class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
@@ -22,5 +43,7 @@
             allow="fullscreen"
             loading="lazy"
         ></iframe>
-    </div>
+         </div>
+        {/if}
+   
 </div>
