0231cb6af56c99c011071105462c0fa4554ce584
vandorena <alexvd@hackclub.com>
2025-08-07 10:24:20 -0400

    :)

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -4,6 +4,83 @@
     function toggleEasyMode() {
         easyMode = !easyMode;
     }
+
+    class StopwatchAPI {
+        constructor() {
+            this.startTime = 0;
+            this.elapsedTime = 0;
+            this.isRunning = false;
+            this.intervalId = undefined;
+        }
+
+        start() {
+            if (!this.isRunning) {
+                this.startTime = Date.now() - this.elapsedTime;
+                this.isRunning = true;
+                this.intervalId = setInterval(() => {
+                    this.elapsedTime = Date.now() - this.startTime;
+                }, 10);
+            }
+        }
+
+        stop() {
+            if (this.isRunning && this.intervalId) {
+                clearInterval(this.intervalId);
+                this.isRunning = false;
+            }
+        }
+
+        reset() {
+            if (this.intervalId) {
+                clearInterval(this.intervalId);
+            }
+            this.startTime = 0;
+            this.elapsedTime = 0;
+            this.isRunning = false;
+        }
+
+        getTime() {
+            return this.elapsedTime;
+        }
+
+        getFormattedTime() {
+            const totalMs = this.elapsedTime;
+            const hours = Math.floor(totalMs / 3600000);
+            const minutes = Math.floor((totalMs % 3600000) / 60000);
+            const seconds = Math.floor((totalMs % 60000) / 1000);
+            const milliseconds = Math.floor((totalMs % 1000) / 10);
+            
+            return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}`;
+        }
+
+        isActive() {
+            return this.isRunning;
+        }
+    }
+
+    let stopwatch = new StopwatchAPI();
+    let displayTime = $state('00:00:00.00');
+
+    $effect(() => {
+        const interval = setInterval(() => {
+            displayTime = stopwatch.getFormattedTime();
+        }, 10);
+
+        return () => clearInterval(interval);
+    });
+
+    function startStopwatch() {
+        stopwatch.start();
+    }
+
+    function stopStopwatch() {
+        stopwatch.stop();
+    }
+
+    function resetStopwatch() {
+        stopwatch.reset();
+        displayTime = stopwatch.getFormattedTime();
+    }
 </script>
 <div class="min-h-screen flex justify-center items-center p-8 relative z-10">
     <a href="/">
@@ -11,14 +88,19 @@
             [ Home ]
         </button>
     </a>
-    <a href="https://matais.me/nsfw/">
+    <a href="https://submit.hackclub.com/armed">
         <button class="absolute top-5 left-10 hover:underline">
             [ Submit ]
         </button>
     </a>
-    <button class="absolute top-5 right-80 hover:underline" on:click={toggleEasyMode}>
+    <button class="absolute top-5 right-80 hover:underline" onclick={toggleEasyMode}>
         {easyMode ? '[ Hard Mode ]' : '[ Easy Mode ]'}
     </button>
+    <h1 class="absolute top-5 justify-center">Remember to Save your code as a .txt file using the save button</h1>
+    <div>{displayTime}</div>
+    <button onclick={startStopwatch}>Start</button>
+    <button onclick={stopStopwatch}>Stop</button>
+    <button onclick={resetStopwatch}>Reset</button>
     {#if !easyMode}
         <a href="https://peterhigginson.co.uk/ARMlite/Programming%20reference%20manual_v1_3.pdf">
             <button class="absolute top-5 left-80 hover:underline">
@@ -30,11 +112,6 @@
             <button class="absolute top-5 left-80 hover:underline">
                 [ Instructionset Guide ]
             </button>
-        </a>
-        <a href="https://pmt.physicsandmathstutor.com/download/Computer-Science/A-level/Past-Papers/AQA/AS-Paper-2/Assembly%20Language%20Instruction%20-%20Paper%202%20AQA%20Computer%20Science%20AS-level.pdf">
-            <button class="absolute top-5 left-80 hover:underline">
-                [ Instructionset Guide ]
-            </button> 
         </a>
     {/if}
         {#if easyMode}
@@ -57,6 +134,6 @@
             loading="lazy"
         ></iframe>
          </div>
-        {/if}
-   
+         {/if}
+
 </div>
