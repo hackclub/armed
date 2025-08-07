3bbedc267094b2ef757cf2ed53eae0ceb4d2563e
vandorena <alexvd@hackclub.com>
2025-08-07 11:50:21 -0400

    it wasn't relative

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -83,57 +83,75 @@
     }
 </script>
 <div class="min-h-screen flex justify-center items-center p-8 relative z-10">
-    <a href="/">
-        <button class="absolute top-5 right-10 hover:underline">
-            [ Home ]
-        </button>
-    </a>
-    <a href="https://submit.hackclub.com/armed">
-        <button class="absolute top-5 left-10 hover:underline">
-            [ Submit ]
-        </button>
-    </a>
-    <button class="absolute top-5 right-40 hover:underline" onclick={toggleEasyMode}>
-        {easyMode ? '[ Hard Mode ]' : '[ Easy Mode ]'}
-    </button>
-    <h1 class="absolute top-5 left-100 underline">Remember to Save your code as a .txt file using the save button, also screenshot the page and it's timer</h1>
-    <div class="absolute top-5 right-150 text-white">{displayTime}</div>
-    <button onclick={startStopwatch} class= "absolute top-5 right-125 hover:underline hover:text-white">[ Start ]</button>
-    <button onclick={stopStopwatch} class= "absolute top-5 right-100 hover:underline hover:text-white">[ Stop ]</button>
-    <button onclick={resetStopwatch} class= "absolute top-5 right-75 hover:underline hover:text-red-500">[ Reset ]</button>
-    {#if !easyMode}
-        <a href="https://peterhigginson.co.uk/ARMlite/Programming%20reference%20manual_v1_3.pdf">
-            <button class="absolute top-5 left-40 hover:underline">
-                [ Instructionset Guide ]
-            </button>
-        </a>
-    {:else}
-        <a href="https://pmt.physicsandmathstutor.com/download/Computer-Science/A-level/Past-Papers/AQA/AS-Paper-2/Assembly%20Language%20Instruction%20-%20Paper%202%20AQA%20Computer%20Science%20AS-level.pdf">
-            <button class="absolute top-5 left-40 hover:underline">
-                [ Instructionset Guide ]
-            </button>
-        </a>
-    {/if}
-        {#if easyMode}
+    {#if easyMode}
         <div class="w-full max-w-4xl h-[900px] relative z-20">
-         <iframe 
-            src="https://www.peterhigginson.co.uk/AQA/?F5=06-Aug-25_17:58:56" 
-            class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
-            title="ArmLite"
-            allow="fullscreen"
-            loading="lazy"
-        ></iframe>
-         </div>
-        {:else}
+            <!-- Buttons positioned relative to iframe container -->
+            <a href="/" class="absolute -top-12 right-0 z-40">
+                <button class="hover:underline">
+                    [ Home ]
+                </button>
+            </a>
+            <a href="https://submit.hackclub.com/armed" class="absolute -top-12 left-0 z-40">
+                <button class="hover:underline">
+                    [ Submit ]
+                </button>
+            </a>
+            <button class="absolute -top-12 right-20 hover:underline z-40" onclick={toggleEasyMode}>
+                {easyMode ? '[ Hard Mode ]' : '[ Easy Mode ]'}
+            </button>
+            <a href="https://pmt.physicsandmathstutor.com/download/Computer-Science/A-level/Past-Papers/AQA/AS-Paper-2/Assembly%20Language%20Instruction%20-%20Paper%202%20AQA%20Computer%20Science%20AS-level.pdf" class="absolute -top-12 left-20 z-40">
+                <button class="hover:underline">
+                    [ Instructionset Guide ]
+                </button>
+            </a>
+            <h1 class="absolute -top-6 left-0 underline text-sm z-40">Remember to Save your code as a .txt file using the save button, also screenshot the page and it's timer</h1>
+            <div class="absolute -top-12 right-96 text-white z-40">{displayTime}</div>
+            <button onclick={startStopwatch} class="absolute -top-12 right-80 hover:underline hover:text-white z-40">[ Start ]</button>
+            <button onclick={stopStopwatch} class="absolute -top-12 right-64 hover:underline hover:text-white z-40">[ Stop ]</button>
+            <button onclick={resetStopwatch} class="absolute -top-12 right-48 hover:underline hover:text-red-500 z-40">[ Reset ]</button>
+            
+            <iframe 
+                src="https://www.peterhigginson.co.uk/AQA/?F5=06-Aug-25_17:58:56" 
+                class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
+                title="ArmLite"
+                allow="fullscreen"
+                loading="lazy"
+            ></iframe>
+        </div>
+    {:else}
         <div class="w-full max-w-8xl h-[900px] relative z-20">
-        <iframe 
-            src="https://peterhigginson.co.uk/ARMlite/" 
-            class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
-            title="ArmLite"
-            allow="fullscreen"
-            loading="lazy"
-        ></iframe>
-         </div>
-         {/if}
-
+            <!-- Buttons positioned relative to iframe container -->
+            <a href="/" class="absolute -top-12 right-0 z-40">
+                <button class="hover:underline">
+                    [ Home ]
+                </button>
+            </a>
+            <a href="https://submit.hackclub.com/armed" class="absolute -top-12 left-0 z-40">
+                <button class="hover:underline">
+                    [ Submit ]
+                </button>
+            </a>
+            <button class="absolute -top-12 right-20 hover:underline z-40" onclick={toggleEasyMode}>
+                {easyMode ? '[ Hard Mode ]' : '[ Easy Mode ]'}
+            </button>
+            <a href="https://peterhigginson.co.uk/ARMlite/Programming%20reference%20manual_v1_3.pdf" class="absolute -top-12 left-20 z-40">
+                <button class="hover:underline">
+                    [ Instructionset Guide ]
+                </button>
+            </a>
+            <h1 class="absolute -top-6 left-0 underline text-sm z-40">Remember to Save your code as a .txt file using the save button, also screenshot the page and it's timer</h1>
+            <div class="absolute -top-12 right-96 text-white z-40">{displayTime}</div>
+            <button onclick={startStopwatch} class="absolute -top-12 right-80 hover:underline hover:text-white z-40">[ Start ]</button>
+            <button onclick={stopStopwatch} class="absolute -top-12 right-64 hover:underline hover:text-white z-40">[ Stop ]</button>
+            <button onclick={resetStopwatch} class="absolute -top-12 right-48 hover:underline hover:text-red-500 z-40">[ Reset ]</button>
+            
+            <iframe 
+                src="https://peterhigginson.co.uk/ARMlite/" 
+                class="w-full h-full border-2 border-fuchsia-800 rounded-lg relative z-30" 
+                title="ArmLite"
+                allow="fullscreen"
+                loading="lazy"
+            ></iframe>
+        </div>
+    {/if}
 </div>
